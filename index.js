5057fa594d57bdfe1f6f11213f58222678c67615
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-05-19 22:10:11 -0300

    Banco de dados implementado no endpoint Update

--- index.js
+++ index.js
@@ -57,29 +57,32 @@ async function main() {
         // }
 
         await collection.insertOne(novoItem)
-        res.status(201).send('Item adicionado com sucesso: ' + novoItem)
+        res.send(novoItem)
     })
 
-    app.put('/personagem/:id', function (req, res) {
+    app.put('/personagem/:id', async function (req, res) {
         const id = req.params.id
 
-        if (!lista[id - 1]) {
-            return res.status(404).send('Item não encontrado.')
-        }
+        // if (!lista[id - 1]) {
+        //     return res.status(404).send('Item não encontrado.')
+        // }
 
-        const body = req.body
-        const novoItem = body.nome
+        const novoItem = req.body
 
-        if (!novoItem) {
+
+        if (!novoItem || !novoItem.nome) {
             return res.status(400).send('Corpo da requisição deve conter a propriedade `nome`.')
         }
 
-        if (lista.includes(novoItem)) {
-            return res.status(409).send('Esse item `' + novoItem + '` já existe na lista.')
-        }
+        // if (lista.includes(novoItem)) {
+        //     return res.status(409).send('Esse item `' + novoItem + '` já existe na lista.')
+        // }
 
-        lista[id - 1] = novoItem
-        res.status(201).send('Item atualizado com sucesso: ' + id + ' - ' + novoItem)
+        await collection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: novoItem }
+        )
+        res.send(novoItem)
     })
 
     app.delete('/personagem/:id', function (req, res) {
